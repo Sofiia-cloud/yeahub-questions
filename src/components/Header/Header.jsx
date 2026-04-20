0b49f5a890e7d6685e998ea63715897731efc8f9
Sofiia-cloud <sofiia.g@icloud.com>
2026-04-20 21:06:32 +0300

    add basic footer and header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <div>
+      <img />
+      <img />
+      <li>
+        <ul>База вопросов</ul>
+        <ul>Тренажер</ul>
+        <ul>Материалы</ul>
+        <ul>Навыки (hh)</ul>
+      </li>
+      <button>Вход</button>
+      <button>Регистрация</button>
+    </div>
+  );
+}
+
+export default Header;
