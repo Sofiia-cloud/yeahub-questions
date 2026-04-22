2331111d7659c3af9162a723aba4c9b51b69a9ed
Sofiia-cloud <sofiia.g@icloud.com>
2026-04-22 21:40:16 +0300

    style header and footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,24 @@
+import styles from "./Header.module.css";
 function Header() {
   return (
-    <div>
-      <img />
-      <img />
-      <li>
-        <ul>База вопросов</ul>
-        <ul>Тренажер</ul>
-        <ul>Материалы</ul>
-        <ul>Навыки (hh)</ul>
-      </li>
-      <button>Вход</button>
-      <button>Регистрация</button>
-    </div>
+    <header>
+      <div className={styles.headerContainer}>
+        <div className={styles.headerContainer__logo}>
+          <img src="src\images\logo.png" alt="Logo image" />
+          <img src="src\images\logo_Yeahub.png" alt="Yeahub logo"/>
+        </div>
+
+        <nav className={styles.headerContainer__links}>
+          <a href="#">База вопросов</a>
+          <a href="#">Тренажер</a>
+          <a href="#">Материалы</a>
+        </nav>
+        <div className={styles.headerContainer__buttons}>
+          <button className={styles.loginBtn}>Вход</button>
+          <button className={styles.registerBtn}>Регистрация</button>
+        </div>
+      </div>
+    </header>
   );
 }
 
