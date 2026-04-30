80b26d8c1f2a7003f12884630f0200fef6501fed
Sofiia-cloud <sofiia.g@icloud.com>
2026-04-30 11:28:00 +0300

    add show answer

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -2,30 +2,50 @@ import { Pagination } from "../Pagination/Pagination";
 import ReactMarkdown from "react-markdown";
 
 import styles from "./Questions.module.css";
+import { useState } from "react";
 function Questions({ questions, pageNumber, setPageNumber }) {
   const totalPages = Math.ceil(questions?.total / questions?.limit);
+  const [openQuestionId, setOpenQuestionId] = useState(null);
+  const toggleQuestion = (id) => {
+    setOpenQuestionId(openQuestionId === id ? null : id);
+  };
   return (
     <div className={styles.questionContainer}>
       <h2>Вопросы</h2>
       <hr />
       <ul>
         {questions?.data.map((question) => {
+          const isOpen = openQuestionId === question.id;
           return (
             <li key={question.id}>
               <h3>{question.title}</h3>
-              <div>
-                <span className={styles.tag_name}>
-                  Рейтинг: <span className={styles.tag}>{question.rate}</span>
-                </span>
-                <span className={styles.tag_name}>
-                  Сложность:{" "}
-                  <span className={styles.tag}>{question.complexity}</span>
-                </span>
-              </div>
-              {question.imageSrc && (
-                <img src={question.imageSrc} alt="Изображение ответа" />
+              <button
+                className={`${styles.arrowButton} ${isOpen ? styles.open : ""}`}
+                onClick={() => toggleQuestion(question.id)}
+                onClick={() => toggleQuestion(question.id)}
+              >
+                {isOpen ? "▲" : "▼"}
+              </button>
+              {isOpen && (
+                <>
+                  <div>
+                    <span className={styles.tag_name}>
+                      Рейтинг:{" "}
+                      <span className={styles.tag}>{question.rate}</span>
+                    </span>
+                    <span className={styles.tag_name}>
+                      Сложность:{" "}
+                      <span className={styles.tag}>{question.complexity}</span>
+                    </span>
+                  </div>
+                  {question.imageSrc && (
+                    <img src={question.imageSrc} alt="Изображение ответа" />
+                  )}
+                  <p
+                    dangerouslySetInnerHTML={{ __html: question.shortAnswer }}
+                  />
+                </>
               )}
-              <p dangerouslySetInnerHTML={{ __html: question.shortAnswer }} />
             </li>
           );
         })}
